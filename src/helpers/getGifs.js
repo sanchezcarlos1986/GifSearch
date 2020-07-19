b528113d786b21743ade73cdeb6071e0a94b327e
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2020-07-19 13:40:45 -0400

    feat(components): added helpers

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+export const getGifs = async (category) => {
+  try {
+    const apiKey = '1YST8FDtdIB5qC8fDVGwNW8V2olj2b92';
+    const baseURL = `http://api.giphy.com/v1/gifs/search?q=${category}&limit=8`;
+
+    const resp = await fetch(`${baseURL}&api_key=${apiKey}`);
+    const {data} = await resp.json();
+
+    const gifs = data.map((img) => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url,
+    }));
+
+    return gifs;
+  } catch (err) {
+    // Error handler
+    console.error('err:', err);
+  }
+};
