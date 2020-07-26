d793de92b29db6463bb61af9d64d7cb78ba71a73
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2020-07-26 18:36:41 -0400

    removed loading from app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import {useFetchGifs} from './hooks/useFetchGifs';
 import ListOfGifs from './components/ListOfGifs';
-import Loading from './components/Loading';
 
 /**
  * App component
@@ -10,11 +8,10 @@ import Loading from './components/Loading';
  * @return {function} Returns a component
  */
 function App() {
-  const {data, loading} = useFetchGifs('city pop');
-
   return (
     <div className="App">
-      {loading ? <Loading /> : <ListOfGifs gifs={data} />}
+      <h1>Giffy</h1>
+      <ListOfGifs category="mega man" />
     </div>
   );
 }
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {useFetchGifs} from '../hooks/useFetchGifs';
+import Loading from './Loading';
 import Gif from './Gif';
 
 /**
@@ -7,16 +9,24 @@ import Gif from './Gif';
  * @constructor
  * @param {array} gifs - List of gifs
  */
-export default function ListOfGifs({gifs}) {
+export default function ListOfGifs({category}) {
+  const {data, loading} = useFetchGifs(category);
+
   return (
     <div className="ListOfGifs">
-      {gifs.map(({id, url, title}) => (
-        <Gif key={id} id={id} url={url} title={title} />
-      ))}
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {data.map(({id, url, title}) => (
+            <Gif key={id} id={id} url={url} title={title} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
 
 ListOfGifs.propTypes = {
-  gifs: PropTypes.array.isRequired,
+  category: PropTypes.string.isRequired,
 };
