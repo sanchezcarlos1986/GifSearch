bd5e6d02aeea70b92c9d18dec6e29d7f1f7997ad
Carlos Sánchez <sanchez.carlos.1986@gmail.com>
2020-07-26 19:08:08 -0400

    testing husky

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -11,7 +11,7 @@ import PropTypes from 'prop-types';
 export default function Gif({id, url, title}) {
   return (
     <a href={`#${id}`}>
-      <h4>{title}</h4>
+      <h4>{title || 'No title ...'}</h4>
       <img src={url} alt={title} />
     </a>
   );
